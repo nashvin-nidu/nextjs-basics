f005b1dad4ea7b5a69673442d12203738b3f76ac
Nashvin Nidu <nashvinnidu1111@gmail.com>
2025-11-05 00:46:20 +0530

    background added

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <p>I need here header</p>
         {children}
       </body>
     </html>
